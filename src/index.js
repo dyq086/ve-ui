8b51a03e9648af49aef0d5ab09019a729f492977
yongqing <490844594@qq.com>
2017-09-15 14:45:42 +0800

    [build] 1.0.0

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ import PaletteButton from '../packages/palette-button';
 import '../src/assets/font/iconfont.css';
 import merge from './utils/merge';
 
-const version = '098';
+const version = '1.0.0';
 const install = function(Vue, config = {}) {
   if (install.installed) return;
 
