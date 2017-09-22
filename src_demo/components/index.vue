66e9fa7fa58ffbbe0095cf9fbecec41ce9bd3523
yongqing <490844594@qq.com>
2017-09-22 15:10:51 +0800

    index

--- src_demo/components/index.vue
+++ src_demo/components/index.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>首页</div>
+  <div>
+    <etc-cell-block>
+      <etc-cell-item arrow v-for="(item,index) in list" :key="index" type="link" :href="item.link">
+        <svg slot="icon" class="icon" aria-hidden="true"><use :xlink:href="item.icon"></use></svg>
+        <span slot="title">{{item.name}}</span>
+      </etc-cell-item>
+    </etc-cell-block>
+  </div>
 </template>
 
 <script>
+  import iconfont from "../assets/icon/iconfont"
   export default {
-   
+    data() {
+      return {
+        list: [{
+            icon: '#icon-yuhangyuan',
+            name: 'Button',
+            link: '/button'
+          },
+          {
+            icon: '#icon-yueqiu',
+            name: 'Cell',
+            link: '/cell'
+          },
+          {
+            icon: '#icon-xingxi',
+            name: 'Switch',
+            link: '/switch'
+          }
+        ]
+      }
+    }
   }
 </script>
 
 <style lang="scss">
-  
+
 </style>
