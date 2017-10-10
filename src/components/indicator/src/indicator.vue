a7114908afe6270a67ac73082a63f38703dff712
yongqing <490844594@qq.com>
2017-10-10 16:49:02 +0800

    Indicator
    Indicator

--- src/components/indicator/src/indicator.vue
+++ src/components/indicator/src/indicator.vue
@@ -1,26 +1,6 @@
 <template>
   <div class="etc-dialog-box">
     <div class="etc-indicator">
-<!--        <div class="spinner">
-        <div class="spinner-container container1">
-          <div class="circle1"></div>
-          <div class="circle2"></div>
-          <div class="circle3"></div>
-          <div class="circle4"></div>
-        </div>
-        <div class="spinner-container container2">
-          <div class="circle1"></div>
-          <div class="circle2"></div>
-          <div class="circle3"></div>
-          <div class="circle4"></div>
-        </div>
-        <div class="spinner-container container3">
-          <div class="circle1"></div>
-          <div class="circle2"></div>
-          <div class="circle3"></div>
-          <div class="circle4"></div>
-        </div>
-      </div> -->
       <div class="circle circle-rotate"></div>
       <div class="title" v-if="mes!==''">{{mes}}</div>
     </div>
