29d9df4bd47358e850491226cd7e660606fdb635
yongqing <490844594@qq.com>
2018-02-01 20:03:18 +0800

    button

--- src/components/button/src/button.vue
+++ src/components/button/src/button.vue
@@ -1,74 +1,66 @@
 <template>
-  <button :disabled="disabled" :class="classObject" :style="{background:bgcolor,color:color,border:borders}">
-    <slot></slot>
-  </button>
+  <button :disabled="disabled" :class="classObject" :style="{background:bgcolor,color:color,border:borders}"><slot></slot></button>
 </template>
-<script>
-//type="text/babel"
-import {
-  isColor
-} from '../../../plug-in/utils'
-export default {
-  name: 'etc-button',
-  props: {
-    disabled: Boolean,
-    type: {
-      validator(value) {
-        return ['default', 'primary', 'danger', , 'disabled'].indexOf(value) > -1;
+
+<script type="text/babel">
+  import {
+    isColor
+  } from '../../../plug-in/utils'
+  export default {
+    name: 'etc-button',
+    props: {
+      disabled: Boolean,
+      type: {
+        validator(value) {
+          return ['default', 'primary', 'danger', , 'disabled'].indexOf(value) > -1;
+        },
+        default: 'default'
       },
-      default: 'default'
-    },
-    size: {
-      validator(value) {
-        return ['small', 'medium'].indexOf(value) > -1;
+      size: {
+        validator(value) {
+          return ['sm', 'md'].indexOf(value) > -1;
+        }
+      },
+      bgcolor: {
+        validator(value) {
+          if (!value) return true;
+          return isColor(value);
+        }
+      },
+      color: {
+        validator(value) {
+          if (!value) return true;
+          return isColor(value);
+        }
+      },
+      borderColor: {
+        validator(value) {
+          if (!value) return true;
+          return isColor(value);
+        }
       }
     },
-    bgcolor: {
-      validator(value) {
-        if (!value) return true;
-        return isColor(value);
+    computed: {
+      classObject() {
+        let a = 'etc-button';
+        let b = 'btn-' + this.type;
+        let c = this.size ? 'size-' + this.size : '';
+        if (this.disabled) {
+          b = 'btn-disable'
+        }
+        return a + ' ' + b + ' ' + c;
+      },
+      borders() {
+        if (this.bgcolor && this.color) {
+          return this.borderColor ? ("1px solid " + this.borderColor) : "none"
+        }
       }
     },
-    color: {
-      validator(value) {
-        if (!value) return true;
-        return isColor(value);
-      }
-    },
-    borderColor: {
-      validator(value) {
-        if (!value) return true;
-        return isColor(value);
-      }
-    },
-    shape: {
-      type: String,
-      default: ''
-    }
-  },
-  computed: {
-    classObject() {
-      let a = 'etc-button'; //基础
-      let b = 'btn-' + this.type;
-      let c = this.size ? 'size-' + this.size : ''; //大小
-      if (this.disabled) { //是否禁用
-        b = 'btn-disable'
-      }
-      let e = this.shape != '' ? 'circle-shape' : '' //是否圆角
-      return a + ' ' + b + ' ' + c + ' ' + e;
-    },
-    borders() {
-      if (this.bgcolor && this.color) {
-        return this.borderColor ? ("1px solid " + this.borderColor) : "none"
-      }
-    }
-  },
-  mounted() {}
-}
-
+    mounted() {}
+  }
 </script>
-<style lang="scss">
-@import '../../../style/necessary.scss';
-@import './button.scss';
 
+<style lang="scss">
+  @import '../../../style/necessary.scss';
+  @import './button.scss';
 </style>
